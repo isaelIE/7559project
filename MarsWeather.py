2707e5fd499c85cd80b17a1eebd1e2edd1ef9472
Phillip <pgiourousis@csumb.edu>
2023-05-11 23:26:29 -0700

    Nasa code takes input accordingly and works as it should, Just might tidy it up a bit to look better

--- MarsWeather.py
+++ MarsWeather.py
@@ -2,7 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from flask import Flask, render_template
 import requests, json
+import io
 from flask_bootstrap import Bootstrap5
+from PIL import Image
+
 
 #nasa = Client('oGb1FfTXlYV5jwNzjGMKeDJ6s4pvSO54FIqcUw2z')
 app = Flask(__name__)
@@ -20,6 +23,9 @@ figure = plt.figure(figsize=(10,5))
 max_temperature =[]
 min_temperature =[]
 
+pressure_readings =[]
+
+
 
 min_temperature.append(data['sols'][2]['min_temp'])
 min_temperature.append(data['sols'][5]['min_temp'])
@@ -30,27 +36,55 @@ max_temperature.append(data['sols'][2]['max_temp'])
 max_temperature.append(data['sols'][5]['max_temp'])
 max_temperature.append(data['sols'][6]['max_temp'])
 
+
+pressure_readings.append(data['sols'][1]['pressure'])
+pressure_readings.append(data['sols'][2]['pressure'])
+pressure_readings.append(data['sols'][3]['pressure'])
+pressure_readings.append(data['sols'][4]['pressure'])
+pressure_readings.append(data['sols'][5]['pressure'])
+
 #print(max_temperature)
 #print(min_temperature)
 
 #create a datasheet
 data ={"MinTempDay2":min_temperature[0], "MinTempDay5":min_temperature[1],"MinTempDay6":min_temperature[2],"MaxTempDay2":max_temperature[0],"MaxTempDay5":max_temperature[1],"MaxTempDay6":max_temperature[2]}
-
+#data2 ={"PressureReading1":pressure_readings[0], "PressureReading2":pressure_readings[1],"PressureReading3":pressure_readings[2],"PressureReading4":pressure_readings[3], "PressureReading5":pressure_readings[4], "PressureReading6":pressure_readings[5]}
 min_temp=list(data.keys())
 max_temp=list(data.values())
 
 
+
 plt.bar(min_temp, max_temp,color='red',width=0.4)
+
 
 
 plt.xlabel("Temperature Readings")
 plt.ylabel("Highs and Lows")
 plt.title("Mars Weather Temperature Reading")
 
-plt.savefig('bargraph.png',dpi=300,bbox_inches='tight')
-plt.show()
 
-#@app.route('/')
-#def main():
+#pressure_readings = list(data2.keys())
+#pressure_readings = list(data2.values())
 
- #   return render_template('mars.html', data=)
+#plt.xlabel("Pressure Readings")
+#plt.ylabel("Pressure throughout days")
+#plt.title("Mars Pressure Readings")
+
+
+#plt.bar(pressure_readings, colo ='blue',width=.4)
+#plt.savefig()
+fig =plt.gcf()
+#plt.savefig('bargraph.png',dpi=300,bbox_inches='tight')
+#plt.show()
+
+def fig2img(fig):
+    buf = io.BytesIO()
+    fig.savefig(buf)
+    buf.seek(0)
+    img = Image.open(buf)
+    return img
+
+@app.route('/')
+def main():
+    img = fig2img(fig)
+    return render_template('mars.html', data=img)
--- NasaBase.py
+++ NasaBase.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask,request, render_template
 import requests, json
 from flask_bootstrap import Bootstrap5
 from datetime import date
 import datetime
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
@@ -10,26 +11,34 @@ bootstrap = Bootstrap5(app)
 # better to replace this with your own key from https://api.nasa.gov/
 my_key = 'D8FJrAVDcE5RHJ29uwD5lRftLXMDO6Tw3iGnj19V'
 
-val = input("Enter the starting date value you want to see Nasa's picture of the day: ") 
 
-val_two = input("Enter the end date value you want to see Nasa's picture of the day: ") 
+
+
 
 
 format = '%Y/%m/%d'
 
-datetime_start = datetime.datetime.strptime(val, format)
-datetime_end = datetime.datetime.strptime(val_two, format)
 
-payload = {
-  'api_key': my_key,
-  'start_date': datetime_start.date(),
-  'end_date': datetime_end.date()
-}
+
 
 endpoint = 'https://api.nasa.gov/planetary/apod'
 
 @app.route('/')
 def main():
+    return render_template('nasa_index.html')
+
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+
+    variable = request.form['date']
+    datetime_start = datetime.datetime.strptime(variable, format)
+    datetime_end = datetime.datetime.strptime(variable, format)
+    payload = {
+    'api_key': my_key,
+    'start_date': datetime_start.date(),
+    'end_date': datetime_end.date()
+    }
     try:
         r = requests.get(endpoint, params=payload)
         data = r.json()
@@ -37,3 +46,4 @@ def main():
     except:
         print('please try again')
     return render_template('nasa_home.html', data=data)
+
